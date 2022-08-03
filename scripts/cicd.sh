a4404feb929d201035f2b18502d12ed13352e575
gaylonalfano <gaylon.alfano@gmail.com>
2022-08-03 10:57:16 +0800

    Attempting to update and use the CICD.sh bash script. I updated the paths to represent my repo. We'll see if I have it right...

--- scripts/cicd.sh
+++ scripts/cicd.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
+# NOTE I've modified this to fit my repo structure. I removed the --bpf-out-dir,
+# so that it keeps the default of target/deploy
 
-SOLANA_PROGRAMS=("sum" "square")
+SOLANA_PROGRAMS=("calculator")
+# SOLANA_PROGRAMS=("sum" "square" "calculator")
 
 case $1 in
     "reset")
@@ -12,23 +15,29 @@ case $1 in
             fi
         done
         for program in "${SOLANA_PROGRAMS[@]}"; do
-            cargo clean --manifest-path=./src/$program/Cargo.toml
+            cargo clean --manifest-path=./src/programs/$program/Cargo.toml
+            # cargo clean --manifest-path=./src/$program/Cargo.toml
         done
-        rm -rf dist/program
+        rm -rf src/programs/$program/target
+        # rm -rf dist/program
         ;;
     "clean")
         rm -rf ./node_modules
         for program in "${SOLANA_PROGRAMS[@]}"; do
-            cargo clean --manifest-path=./src/$program/Cargo.toml
+            cargo clean --manifest-path=./src/programs/$program/Cargo.toml
+            # cargo clean --manifest-path=./src/$program/Cargo.toml
         done;;
     "build")
         for program in "${SOLANA_PROGRAMS[@]}"; do
-            cargo build-bpf --manifest-path=./src/$program/Cargo.toml --bpf-out-dir=./dist/program
+            cargo build-bpf --manifest-path=./src/programs/$program/Cargo.toml
+            # cargo build-bpf --manifest-path=./src/$program/Cargo.toml --bpf-out-dir=./dist/program
         done;;
     "deploy")
         for program in "${SOLANA_PROGRAMS[@]}"; do
-            cargo build-bpf --manifest-path=./src/$program/Cargo.toml --bpf-out-dir=./dist/program
-            solana program deploy dist/program/$program.so
+            cargo build-bpf --manifest-path=./src/programs/$program/Cargo.toml
+            solana program deploy src/programs/$program/target/deploy/$program.so
+            # cargo build-bpf --manifest-path=./src/$program/Cargo.toml --bpf-out-dir=./dist/program
+            # solana program deploy dist/program/$program.so
         done;;
     "reset-and-build")
         rm -rf ./node_modules
@@ -38,11 +47,16 @@ case $1 in
                 solana program close $x; 
             fi
         done
-        rm -rf dist/program
+        rm -rf src/programs/$program/target
+        # rm -rf dist/program
         for program in "${SOLANA_PROGRAMS[@]}"; do
-            cargo clean --manifest-path=./src/$program/Cargo.toml
-            cargo build-bpf --manifest-path=./src/$program/Cargo.toml --bpf-out-dir=./dist/program
-            solana program deploy dist/program/$program.so
+            cargo clean --manifest-path=./src/programs/$program/Cargo.toml
+            cargo build-bpf --manifest-path=./src/programs/$program/Cargo.toml
+            solana program deploy src/programs/$program/target/deploy/$program.so
+
+            # cargo clean --manifest-path=./src/$program/Cargo.toml
+            # cargo build-bpf --manifest-path=./src/$program/Cargo.toml --bpf-out-dir=./dist/program
+            # solana program deploy dist/program/$program.so
         done
         npm install
         solana program show --programs
